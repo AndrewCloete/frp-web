fcee8d1984e6bf7f930cb447fc39320698442419
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2024-08-10 14:05:34 +0200

    Basic view

--- src/App.tsx
+++ src/App.tsx
@@ -11,58 +11,97 @@ async function getRecipes(): Promise<m.Recipe[]> {
   const tasks_data = (await response.json()) as m.Recipe[];
   return tasks_data;
 }
+
+function Ingredients(props: { r: m.Recipe }) {
+  return (
+    <div className="RecipeInTable">
+      <table>
+        <tbody>
+          {props.r.ingredients.map((i) => {
+            return (
+              <tr key={i.name}>
+                <td>{i.name}</td>
+                <td>
+                  {JSON.stringify(i.qty)
+                    .replaceAll('"', "")
+                    .replace("{", "")
+                    .replace(":", " ")
+                    .replace("}", "")}
+                  {i.note ? `(${i.note})` : null}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function Instructions(props: { r: m.Recipe }) {
+  return (
+    <div className="RecipeInstructions">
+      {props.r.instructions.split("\n").map((l) => {
+        return <div key={l}>{l}</div>;
+      })}
+    </div>
+  );
+}
+
+function Title(props: { r: m.Recipe }) {
+  return (
+    <div className="RecipeTitle">
+      <div>
+        <b>{props.r.name}</b>
+      </div>
+      <div>Serves {props.r.serves}</div>
+    </div>
+  );
+}
+
 function RecipeLine(props: { r: m.Recipe }) {
   let [open, setOpen] = useState<boolean>(false);
+  function flip() {
+    setOpen(!open);
+  }
   return (
-    <div onClick={() => setOpen(!open)}>
+    <div className="RecipeLine">
       {open ? (
-        <Recipe r={props.r}></Recipe>
+        <Recipe r={props.r} click={flip}></Recipe>
       ) : (
-        <RecipeListItem r={props.r}></RecipeListItem>
+        <RecipeListItem r={props.r} click={flip}></RecipeListItem>
       )}
     </div>
   );
 }
 
-function Recipe(props: { r: m.Recipe }) {
+function Recipe(props: { r: m.Recipe; click: () => void }) {
   return (
     <div className="Recipe">
-      <div className="RecipeTitle">
-        <div>{props.r.name}</div>
-        <div>Serves {props.r.serves}</div>
+      <div onClick={props.click}>
+        <Title r={props.r}></Title>
       </div>
-      <div className="RecipeInTable">
-        <table>
-          <tbody>
-            {props.r.ingredients.map((i) => {
-              return (
-                <tr key={i.name}>
-                  <td>{i.name}</td>
-                  <td>
-                    {JSON.stringify(i.qty)
-                      .replaceAll('"', "")
-                      .replace("{", "")
-                      .replace(":", " ")
-                      .replace("}", "")}
-                    {i.note ? `(${i.note})` : null}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className="RecipeInstructions">
-        {props.r.instructions.split("\n").map((l) => {
-          return <div key={l}>{l}</div>;
-        })}
-      </div>
+      <Ingredients r={props.r}></Ingredients>
+      <Instructions r={props.r}></Instructions>
     </div>
   );
 }
 
-function RecipeListItem(props: { r: m.Recipe }) {
-  return <div className="RecipeListItem">{props.r.name}</div>;
+function RecipeListItem(props: { r: m.Recipe; click: () => void }) {
+  return (
+    <div className="RecipeListItem">
+      <div onClick={props.click}>
+        <Title r={props.r}></Title>
+      </div>
+      <div className="RecipeListImageDiv">
+        <img
+          className="RecipeListImage"
+          src={`img/${props.r.name}.jpg`}
+          alt=""
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 function Recipes(props: { recipes: m.Recipe[] }) {
